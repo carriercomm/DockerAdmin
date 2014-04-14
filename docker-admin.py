d597afb5d4b63b054abbefa4a55980d987a2e606
Dermot O'Sullivan <dermot@rosstech.ie>
2014-04-14 22:52:25 +0100

    Refactorings.

--- docker-admin.py
+++ docker-admin.py
@@ -1,8 +1,7 @@
-import datetime
-import docker
-
 from flask import Flask, jsonify
 from flask.ext.mako import MakoTemplates, render_template
+
+from .utils import getContainers, getImages, isDockerRunning
 
 app = Flask(__name__)
 app.config['PROPAGATE_EXCEPTIONS'] = True
@@ -11,62 +10,36 @@ app.template_folder = "templates"
 mako = MakoTemplates(app)
 
 
-def getContainers(client, all=False):
-    containers = []
-    try:
-        containers = client.containers(all=all)
-    except Exception:
-        pass
-
-    return containers
-
-
 @app.route('/images')
 def images():
-    client = docker.Client()
-    images = []
-
-    try:
-        currentImages = client.images()
-        for image in currentImages:
-            betterImage = {}
-            betterImage['Repositories'] = list(set([repo.split(':')[0] for repo in image['RepoTags']]))
-            betterImage['Tags'] = [repo.split(':')[1] for repo in image['RepoTags']]
-            betterImage['Image'] = image['Id'][:12]
-            betterImage['Created'] = datetime.datetime.fromtimestamp(image['Created']).strftime("%Y-%m-%d %H:%M:%S")
-            betterImage['Size'] = "{0:.1f} MB".format(image['VirtualSize'] / 1000.0 / 1000.0)
-            images.append(betterImage)
-    except Exception:
-        pass
-
-    images = {"images": images}
+    """ View for docker images
+    """
+    images = {"images": getImages()}
     return jsonify(images)
 
 
 @app.route('/containers')
 def containers():
-    client = docker.Client()
-    containers = getContainers(client, all=True)
-    for container in containers:
-        container['Id'] = container['Id'][:12]
-        container['Created'] = datetime.datetime.fromtimestamp(container['Created']).strftime("%Y-%m-%d %H:%M:%S")
-
-    containers = {"containers": containers}
+    """ View for docker containers
+    """
+    containers = {"containers": getContainers(all=True)}
     return jsonify(containers)
 
 
 @app.route('/status')
 def status():
-    client = docker.Client()
-    containers = getContainers(client, all=True)
-
-    status = "Running" if containers else "Stopped"
-    statusColour = "green" if containers else "red"
+    """ Gets the status of docker.
+    """
+    isUp = isDockerRunning()
+    status = "Running" if isUp else "Stopped"
+    statusColour = "green" if isUp else "red"
 
     return jsonify({"status": status, "colour": statusColour})
 
 @app.route('/')
 def index():
+    """ Homepage
+    """
     return render_template('index.mako')
 
 
---
+++ utils.py
@@ -0,0 +1,63 @@
+import docker
+import datetime
+
+
+def datetimeFromTimestamp(timestamp):
+    """ Returns a datetime object from a given timestamp
+    """
+    return datetime.datetime.fromtimestamp(timestamp).strftime("%Y-%m-%d %H:%M:%S")
+
+
+def getContainers(all=False, statusCheck=False):
+    """ Returns a list of container objects in JSON
+    """
+    client = docker.Client()
+    containers = []
+
+    try:
+        containers = client.containers(all=all)
+        if not statusCheck:
+            for container in containers:
+                container['Id'] = container['Id'][:12]
+                container['Created'] = datetime.datetime.fromtimestamp(container['Created']).strftime("%Y-%m-%d %H:%M:%S")
+    except Exception:
+        pass
+
+    return containers
+
+
+def getImages():
+    """ Returns a list of image objects in JSON
+    """
+    client = docker.Client()
+    images = []
+
+    try:
+        currentImages = client.images()
+        for image in currentImages:
+            betterImage = {}
+            betterImage['Repositories'] = list(set([repo.split(':')[0] for repo in image['RepoTags']]))
+            betterImage['Tags'] = [repo.split(':')[1] for repo in image['RepoTags']]
+            betterImage['Image'] = image['Id'][:12]
+            betterImage['Created'] = datetimeFromTimestamp(image['Created'])
+            betterImage['Size'] = "{0:.1f} MB".format(image['VirtualSize'] / 1000.0 / 1000.0)
+            images.append(betterImage)
+    except Exception:
+        pass
+
+    return images
+
+
+def isDockerRunning():
+    """ Returns True if docker is running
+    """
+    isUp = False
+    client = docker.Client()
+
+    try:
+        client.containers(all=all)
+        isUp = True
+    except Exception:
+        pass
+
+    return isUp
